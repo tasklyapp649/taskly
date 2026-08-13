414139b7f2735a4fa10012d26eb8f28aafd967ee
tasklyapp649 <taskly30@gmail.com>
2026-08-13 20:40:32 +0200

    Connexion de l'inscription à Supabase

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://knhyjdpedwestkfvvpbf.supabase.co";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_DHEuzHBOKkIEFXjho5kY4g_JCwFCrKk";
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_PUBLISHABLE_KEY
+);
