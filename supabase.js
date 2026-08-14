7ac723e00b8e861bf06056650aef772d7f89f148
tasklyapp649 <taskly30@gmail.com>
2026-08-14 17:06:20 +0200

    supabase.js modifications

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = "https://knhyjdpedwestkfvvpbf.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_DHEuzHBOKkIEFXj- ho5-kY4g_JCwFCrKk";
+const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_DHEuzHBOKkIEFXj-ho5-kY4g_JCwFCrKk";
 
 window.tasklySupabase = window.supabase.createClient(
   SUPABASE_URL,
